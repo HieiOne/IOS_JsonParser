fafbeac07a7df3ca6ac110a92b35ef80dd8a66c5
Ivan Blasco Gascon <blascogasconiban@gmail.com>
2018-04-03 16:02:54 +0200

    Made it modular

--- modules/parser.py
+++ modules/parser.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 import tkinter as tk
 from tkinter import filedialog #For the window
 from prettytable import PrettyTable #Table
@@ -9,6 +7,10 @@ TEAMS_TOTAL = [] #Full time list
 TEAMS_1ST_HALF = [] #1st time list
 TEAMS_2ND_HALF = [] #2nd time list
 PLAYERS = [] #Full time players list
+
+#INFO STORED:
+# 0 -> name 1 -> possession | 2 -> corners | 3 -> distance covered | 4 -> offsides | 5 -> goals | 6 -> shots | 7 -> shots on goal
+# 8 -> shot accuracy | 9 -> interceptions | 10 -> passes | 11 -> passes accuracy | 12 -> fouls | 13 -> y cards | 14 -> red cards | 15 -> saves
 
 #STATISTICS TYPES
 RED_CARDS = 0
@@ -43,44 +45,119 @@ def getJsonFile(): #Simple function to ask the user where the JSON file at
     file_path = filedialog.askopenfilename(filetypes = (("JSON files", "*.json"),("All files", "*.*") )) #Window and the extensions for an easier search
     return file_path #We return the file path
 
-def parseJson(JsonFile): #Parsing the JSON
-    with open(JsonFile) as data_file:
-        data = json.load(data_file)
-    fullTimeTeams(data)
+def statsInsert(teamName, PosTotal, JsonData, dataList):
+    possession = round(JsonData[POSSESSION]/PosTotal*100,1)
+    corners = JsonData[CORNERS]
+    distanceCovered = round(JsonData[DISTANCE_COVERED]/1000,1)
+    offsides = JsonData[OFFSIDES]
+    goals = JsonData[GOALS]
+    shots = JsonData[SHOTS]
+    shotsOnGoal = JsonData[SHOTS_ON_GOAL]
+    shotAccuracy = 0 #DEFAULT VALUE
+    interceptions = JsonData[INTERCEPTIONS]
+    passes = JsonData[PASSES]
+    passesAccuracy = 0 #DEFAULT VALUE
+    fouls = JsonData[FOULS]
+    yellowCards = JsonData[YELLOW_CARDS]
+    redCards = JsonData[RED_CARDS]
+    saves = JsonData[KEEPER_SAVES]
+
+    if shots >= 1:
+        shotAccuracy = round(shotsOnGoal/shots*100,1)
+    if passes >= 1:
+        passesAccuracy = round(JsonData[PASSES_COMPLETED]/passes*100,1)
+
+    teamList = [teamName,possession,corners,distanceCovered,offsides,goals,shots,shotsOnGoal,shotAccuracy,interceptions,passes,passesAccuracy,fouls,yellowCards,redCards,saves]
+    dataList.append(teamList)
 
 def fullTimeTeams(data):
     PosTotal = 0
-
+    
     for item in range(len(data["matchData"]["teams"])):
         PosTotal += data["matchData"]["teams"][item]["matchTotal"]["statistics"][POSSESSION]
     
     for item in range(len(data["matchData"]["teams"])):
         JsonData = data["matchData"]["teams"][item]["matchTotal"]["statistics"]
-        
-        #MAYBE I COULD PUT THIS ALL IN A METHOD
         teamName = data["matchData"]["teams"][item]["matchTotal"]["name"]
-        possession = round(JsonData[POSSESSION]/PosTotal*100,1)
-        corners = JsonData[CORNERS]
-        distanceCovered = round(JsonData[DISTANCE_COVERED]/1000,1)
-        offsides = JsonData[OFFSIDES]
-        goals = JsonData[GOALS]
-        shots = JsonData[SHOTS]
-        shotsOnGoal = JsonData[SHOTS_ON_GOAL]
-        shotAccuracy = 1 #TODO
-        interceptions = JsonData[INTERCEPTIONS]
-        passes = JsonData[PASSES]
-        passesAccuracy = 1 #TODO
-        fouls = JsonData[FOULS]
-        yellowCards = JsonData[YELLOW_CARDS]
-        redCards = JsonData[RED_CARDS]
-        saves = JsonData[KEEPER_SAVES]
+        statsInsert(teamName,PosTotal,JsonData,TEAMS_TOTAL)
 
-        teamList = [teamName,possession,corners,distanceCovered,offsides,goals,shots,shotsOnGoal,shotAccuracy,interceptions,passes,passesAccuracy,fouls,yellowCards,redCards,saves]
-        TEAMS_TOTAL.append(teamList)
+def halfTimeTeams(data):
+    HALFS_LIST = [TEAMS_1ST_HALF, TEAMS_2ND_HALF]
+    for half in range(len(data["matchData"]["teams"][0]["matchPeriods"])):
+        PosTotal = 0
+        for item in range(len(data["matchData"]["teams"])):
+            PosTotal += data["matchData"]["teams"][item]["matchPeriods"][half]["statistics"][POSSESSION]
+        
+        for item in range(len(data["matchData"]["teams"])):
+            JsonData = data["matchData"]["teams"][item]["matchPeriods"][half]["statistics"]
+            teamName = data["matchData"]["teams"][item]["matchTotal"]["name"]
+            statsInsert(teamName,PosTotal,JsonData,HALFS_LIST[half])
+
+def playersFullTime(data):
+    PosTotal = 0
+    for player in range(len(data["matchData"]["players"])): #For every player
+        for period in range(len(data["matchData"]["players"][player]["matchPeriodData"])): #And for every period
+            PosTotal += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][22]
     
+    JsonData = data["matchData"]["players"][player]["matchPeriodData"][period]['statistics']
+    for player in range(len(data["matchData"]["players"])):
+        playerName = data["matchData"]["players"][player]["info"]["name"]
 
+        possession = 0
+        corners = 0
+        distanceCovered = 0
+        offsides = 0
+        goals = 0
+        shots = 0
+        shotsOnGoal = 0
+        shotAccuracy = 0 #DEFAULT VALUE
+        interceptions = 0
+        passes = 0
+        passesAccuracy = 0 # DEFAULT VALUE
+        passesCompleted = 0
+        fouls = 0
+        yellowCards = 0
+        redCards = 0
+        saves = 0
+        
+        for period in range(len(data["matchData"]["players"][player]["matchPeriodData"])):
+            possession += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][POSSESSION]
+            corners += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][CORNERS]
+            distanceCovered += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][DISTANCE_COVERED]
+            offsides += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][OFFSIDES]
+            goals += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][GOALS]
+            shots += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][SHOTS]
+            shotsOnGoal += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][SHOTS_ON_GOAL]
+            interceptions += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][INTERCEPTIONS]
+            passes += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][PASSES]
+            passesCompleted += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][PASSES_COMPLETED]
+            fouls += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][FOULS]
+            yellowCards += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][YELLOW_CARDS]
+            redCards += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][RED_CARDS]
+            saves += data["matchData"]["players"][player]["matchPeriodData"][period]['statistics'][KEEPER_SAVES]
 
-#JsonFile = getJsonFile()
-JsonFile = 'd:/REPOSITORY/IOS_JsonParser/2018.03.20_20h.42m.07s_nextgen-vs-phoenix_5-1.json'
-#JsonFile = 'd:/REPOSITORY/IOS_JsonParser/2018.04.02_16h.19m.43s_phoenix-vs-nextgen_0-2.json'
-parseJson(JsonFile)
+        possession = round(possession/PosTotal*100,1)
+        distanceCovered = round(distanceCovered/1000,1)
+        if shots >= 1:
+            shotAccuracy = round(shotsOnGoal/shots*100,1)
+        if passes >= 1:
+            passesAccuracy = round(passesCompleted/passes*100,1)
+        teamList = [playerName,possession,corners,distanceCovered,offsides,goals,shots,shotsOnGoal,shotAccuracy,interceptions,passes,passesAccuracy,fouls,yellowCards,redCards,saves]
+        PLAYERS.append(teamList)
+
+def printTable(dataList):
+    table = PrettyTable(['NAME', 'POS', 'CORNERS', 'D_COVERED', 'OFFSIDES', 'GOALS', 'SHOTS', 'SHOTS ON GOAL', 'SHOT  %', 'INTERCEP', 'PASSES', 'PASS %', 'FOULS', 'YELLOW', 'RED', 'SAVES'])
+    for item in dataList:
+        table.add_row([
+            item[0],item[1],item[2],item[3],item[4],item[5],item[6],item[7],
+            item[8],item[9],item[10],item[11],item[12],item[13],item[14],item[15]
+        ])
+    print(table)
+
+def parseJson(JsonFile): #Parsing the JSON
+    with open(JsonFile) as data_file:
+        data = json.load(data_file)
+    fullTimeTeams(data) #Parse full time
+    halfTimeTeams(data) #Parse half times
+    playersFullTime(data) #Parse players
+    return TEAMS_TOTAL, TEAMS_1ST_HALF, TEAMS_2ND_HALF, PLAYERS
